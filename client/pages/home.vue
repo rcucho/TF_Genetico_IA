ed66fb410f92fe2b3bb39de34579241b560fa283
Edinson Cabrera Ñique <urielkillian2607@hotmail.com>
2021-11-25 18:43:28 -0500

    Client

---
+++ client/pages/home.vue
@@ -0,0 +1,148 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div>
+    <div class="relative bg-indigo-800">
+      <div class="absolute inset-0">
+        <img
+          class="w-full h-full object-cover"
+          src="https://www.upc.edu.pe/nosotros/campus/img/campus-san-miguel.jpg"
+          alt=""
+        />
+        <div
+          class="absolute inset-0 bg-indigo-800 mix-blend-multiply"
+          aria-hidden="true"
+        />
+      </div>
+      <div
+        class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8"
+      >
+        <h1
+          class="
+            text-4xl
+            font-extrabold
+            tracking-tight
+            text-white
+            sm:text-5xl
+            lg:text-6xl
+          "
+        >
+          En Desarrollo
+        </h1>
+      </div>
+    </div>
+    <section class="text-gray-600 body-font relative w-full">
+      <div class="absolute inset-0 bg-gray-300">
+        <iframe
+          width="100%"
+          height="100%"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          title="map"
+          scrolling="no"
+          src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          style=""
+        ></iframe>
+      </div>
+      <div class="container px-5 py-24 mx-auto flex">
+        <div
+          class="
+            lg:w-1/3
+            md:w-1/2
+            bg-white
+            rounded-lg
+            p-8
+            flex flex-col
+            md:ml-auto
+            w-full
+            mt-10
+            md:mt-0
+            relative
+            z-10
+            shadow-md
+          "
+        >
+          <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
+            Colocar Datos
+          </h2>
+          <p class="leading-relaxed mb-5 text-gray-600">
+            Post-ironic portland shabby chic echo park, banjo fashion axe
+          </p>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-600"
+              >Punto de Inicio</label
+            >
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="
+                w-full
+                bg-white
+                rounded
+                border border-gray-300
+                focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200
+                text-base
+                outline-none
+                text-gray-700
+                py-1
+                px-3
+                leading-8
+                transition-colors
+                duration-200
+                ease-in-out
+              "
+            />
+          </div>
+          <div class="relative mb-4">
+            <label for="email" class="leading-7 text-sm text-gray-600"
+              >Punto De Destino</label
+            >
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="
+                w-full
+                bg-white
+                rounded
+                border border-gray-300
+                focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200
+                text-base
+                outline-none
+                text-gray-700
+                py-1
+                px-3
+                leading-8
+                transition-colors
+                duration-200
+                ease-in-out
+              "
+            />
+          </div>
+          <button
+            class="
+              text-white
+              bg-indigo-500
+              border-0
+              py-2
+              px-6
+              focus:outline-none
+              hover:bg-indigo-600
+              rounded
+              text-lg
+            "
+          >
+            Confirmar
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,149 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="bg-gray-900">
+    <header class="bg-white">
+      <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div
+          class="
+            w-full
+            py-6
+            flex
+            items-center
+            justify-between
+            border-b border-indigo-500
+            lg:border-none
+          "
+        >
+          <div class="flex items-center">
+            <a href="#">
+              <span class="sr-only">Workflow</span>
+              <img
+                class="h-20 w-auto"
+                src="https://estudiaperu.pe/wp-content/uploads/2021/04/UPC-carreras-para-gente-que-trabaja.png"
+                alt=""
+              />
+            </a>
+            <div class="hidden ml-10 space-x-8 lg:block">
+              <a
+                v-for="link in navigation"
+                :key="link.name"
+                :href="link.href"
+                class="text-base font-medium text-white hover:text-indigo-50"
+              >
+                {{ link.name }}
+              </a>
+            </div>
+          </div>
+          <div class="ml-10 space-x-4">
+            <NuxtLink to="/home">
+              <a
+                href="#"
+                class="
+                  inline-block
+                  bg-gray-800
+                  py-2
+                  px-4
+                  border border-transparent
+                  rounded-md
+                  text-base
+                  font-medium
+                  text-white
+                  hover:bg-gray-900
+                "
+                >Ingresar</a
+              >
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+          <a
+            v-for="link in navigation"
+            :key="link.name"
+            :href="link.href"
+            class="text-base font-medium text-white hover:text-indigo-50"
+          >
+            {{ link.name }}
+          </a>
+        </div>
+      </nav>
+    </header>
+    <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+      <div class="space-y-12">
+        <div
+          class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none"
+        >
+          <h2
+            class="
+              text-3xl
+              font-extrabold
+              text-white
+              tracking-tight
+              sm:text-4xl
+            "
+          >
+            Conoce nuestro equipo
+          </h2>
+          <p class="text-xl text-gray-300">
+            Nuestro equipo está conformado por:
+          </p>
+        </div>
+        <ul
+          role="list"
+          class="
+            space-y-4
+            sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0
+            lg:grid-cols-3 lg:gap-8
+          "
+        >
+          <li
+            v-for="person in people"
+            :key="person.name"
+            class="
+              py-10
+              px-6
+              bg-gray-800
+              text-center
+              rounded-lg
+              xl:px-10 xl:text-left
+            "
+          >
+            <div class="space-y-6 xl:space-y-10">
+              <div class="space-y-2 xl:flex xl:items-center xl:justify-between">
+                <div class="font-medium text-lg leading-6 space-y-1">
+                  <h3 class="text-white">{{ person.name }}</h3>
+                  <p class="text-indigo-400">{{ person.role }}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    people: [
+      {
+        name: 'Edinson Ricardo Cabrera Ñique (U20181E128)',
+        role: 'Ingeniero De Software',
+      },
+      {
+        name: 'Joaquin Echegoaga Ramos (U201715418)',
+        role: 'Ingeniero De Software',
+      },
+      {
+        name: 'Richard Hanns Cucho Landeo (U20181C448)',
+        role: 'Ingeniero De Software',
+      },
+      {
+        name: 'Angie Nicolle Tomasto Cahuana (U201817853)',
+        role: 'Ingeniero De Software',
+      },
+    ],
+    navigation: [],
+  }),
+}
+</script>
